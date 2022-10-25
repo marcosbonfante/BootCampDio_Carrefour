8f8492d7ca26a3a92d7dd78de0d2bafa565dd091
marcosbonfante <marcos.bonfante@gmail.com>
2022-10-25 17:57:52 -0300

    EOL Unix

--- create_apache.sh
+++ create_apache.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+
 echo 'Atualizando sistema..;.'
  apt-get update  -y
  apt-get upgrade -y
@@ -9,7 +9,7 @@ echo 'Instalando pacotes';
  apt-get install apache2 -y
  apt-get install wget -y
  systemctl enable apache2
- 
+
 echo 'baixando e descompactando app...';
  cd  /tmp
  wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
@@ -22,5 +22,5 @@ echo 'Copiando arquivos...'
 echo 'limpando lixo...'
  rm -f /tmp/main.zip
  rm -rf /tmp/app
- 
+
 echo 'Finalizado !!'
